43f88b44623b6b6b8394802d071a444cce6172ea
khusanmeliev <khusanmeliev@gmail.com>
2022-06-21 23:10:19 +0500

    added background image

--- src/Pages/Home/Home.style.js
+++ src/Pages/Home/Home.style.js
@@ -1,12 +1,28 @@
+import background from '../../assets/img/background.jpg';
+import backgroundMobile from '../../assets/img/backgroundMobile.jpg';
+
 import styled from 'styled-components';
+import { mobile } from '../../assets/styles/sizes';
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  gap: 20px;
   height: 80vh;
-  padding: 40px 20px 80px 20px;
   margin-top: 80px;
-  background-color: #fbfbfb;
+  padding: 20px 80px 80px 80px;
+  background-image: linear-gradient(rgba(13, 8, 54, 0.9), rgba(13, 8, 54, 0.9)), url(${background});
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  ${mobile.lg} {
+    padding: 20px 20px 80px 20px;
+    background-image: linear-gradient(rgba(13, 8, 54, 0.9), rgba(13, 8, 54, 0.8)), url(${backgroundMobile});
+  }
+`;
+
+export const ImageLogo = styled.img`
+  width: 200px;
 `;
